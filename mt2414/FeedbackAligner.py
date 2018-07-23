e96303e9a9773b68f5522e8392d764c48343e77c
kavitharaju <kavitharaju18@gmail.com>
2018-07-23 13:43:37 +0530

    Separate tables for auto-aligned and user-corrected
    - new fetch_alignment(lid,table) method returning
    1. the list of source words(strongs)
    2. the list of target(hin) words
    3. machine made alignments as `auto_alignments`
    4. user corrected alignments as `user_alignments`
    5. relevant entries from feedback look up as `feedback suggestions`
    - corrections on confidence formula

--- mt2414/FeedbackAligner.py
+++ mt2414/FeedbackAligner.py
@@ -56,15 +56,18 @@ class FeedbackAligner:
 		try:	
 			cur = self.db.cursor()
 
+
 			# for calculating confidence_score
-			cur.execute("select count(distinct left(occurences,5)) from grk_bible_concordance where word='"+src_word+"'") 
+			cur.execute("select count(distinct left(occurences,5)) from "+self.src_table_name+" where word='"+src_word+"'") 
 			if cur.rowcount>0:
 				total_verses_src_word_occured = cur.fetchone()[0]
 				print(total_verses_src_word_occured)
 			else:
 				total_verses_src_word_occured = 1
 
-			cur.execute(" select count(distinct left(hin_bible_concordance.occurences,5)) from hin_bible_concordance INNER JOIN grk_bible_concordance on left(hin_bible_concordance.occurences,5)=left(grk_bible_concordance.occurences,5) where grk_bible_concordance.word='"+src_word+"' and hin_bible_concordance.word='"+trg_word+"'")
+			query = " select count(distinct left("+ self.trg_table_name +".occurences,5)) from "+ self.trg_table_name +" INNER JOIN "+ self.src_table_name +" on left("+self.trg_table_name+".occurences,5)=left("+self.src_table_name+".occurences,5) where "+self.src_table_name+".word=\'"+src_word+"\' and "+self.trg_table_name+".word=\'"+trg_word+"\'"
+			print(query)
+			cur.execute(query)
 			if cur.rowcount>0:
 				total_verses_trg_word_cooccured = cur.fetchone()[0]
 				print(total_verses_trg_word_cooccured)
@@ -72,16 +75,23 @@ class FeedbackAligner:
 				total_verses_trg_word_cooccured = 0
 
 
-			cur.execute("	select count(distinct b.lid) from grk_bible_concordance a, grk_hin_alignment b, hin_bible_concordance c where a.occurences=b.source_wordID and c.occurences=b.target_wordID and a.word='"+src_word+"' and c.word='"+trg_word+"'")
+			cur.execute("	select count(distinct b.lid) from "+self.src_table_name+" a, "+self.alignment_table_name+" b, "+self.trg_table_name+" c where a.occurences=b.source_wordID and c.occurences=b.target_wordID and a.word='"+src_word+"' and c.word='"+trg_word+"'")
 			if cur.rowcount>0:
 				total_verses_src_trg_aligned = cur.fetchone()[0]
 				print(total_verses_src_trg_aligned)
 			else:
-				total_verses_src_trg_aligned = 0
+				total_verses_src_trg_aligned = 1
 
+
+			cur.execute("	select count(distinct b.lid) from "+self.src_table_name+" a, "+self.alignment_table_name+" b, "+self.trg_table_name+" c where a.occurences=b.source_wordID and c.occurences=b.target_wordID and a.word='"+src_word+"' and c.word!='"+trg_word+"'")
+			if cur.rowcount>0:
+				total_verses_src_trg_NOTaligned = cur.fetchone()[0]
+				print(total_verses_src_trg_NOTaligned)
+			else:
+				total_verses_src_trg_NOTaligned = 0
 
 			co_occurence_confidence = total_verses_trg_word_cooccured/total_verses_src_word_occured
-			aligned_confidence = total_verses_src_trg_aligned/total_verses_trg_word_cooccured
+			aligned_confidence = (total_verses_src_trg_aligned-total_verses_src_trg_NOTaligned) /total_verses_src_trg_aligned
 
 
 			confidence_score = 0.75*co_occurence_confidence + 0.25*aligned_confidence
@@ -95,66 +105,19 @@ class FeedbackAligner:
 			print("Warning: word pair not inserted to look up table")
 			print(e)
 
-	def update_all_alignment_by_word(self,src_word_to_update):
+
+
+	def save_alignment(self,lid,word_pairs,user):
 		cur = self.db.cursor()
 
-		trg_words_to_update = []
-		cur.execute("SELECT target_word from "+self.FeedbackLookup_table_name+" WHERE source_word='"+src_word_to_update+"'")
-		for row in cur.fetchall():
-			trg_words_to_update.append(row[0])
-
-		# print(trg_words_to_update)
-
-		src_positions = []
-		cur.execute("SELECT occurences from "+self.src_table_name+" WHERE word='"+src_word_to_update+"'")
-		for row in cur.fetchall():
-			src_positions.append(row[0])
-
-		# print(src_positions)
-
-
-		trg_positions = []
-		temp = src_positions
-		src_positions = []
-		for pos in temp:
-			flag = "Not found"
-			verseID = re.split("_",pos)[0]
-			cur.execute("SELECT target_wordID from "+self.alignment_table_name+" WHERE source_wordID like'"+verseID+"\_%' and corrected=0" )
-			for target_wordID in cur.fetchall():
-				if target_wordID[0].endswith("255"):
-					continue
-				cur2 = self.db.cursor()
-				cur2.execute("SELECT word from "+self.trg_table_name+" WHERE occurences='"+target_wordID[0]+"'")
-				if cur2.rowcount == 0:
-					continue
-				trg_word = cur2.fetchone()[0]
-				# print("Checking "+trg_word+" at "+target_wordID[0])
-				if trg_word in trg_words_to_update and target_wordID not in trg_positions:
-						trg_positions.append(target_wordID[0])
-						flag = "Found"
-						# print("FOUND ONE")
-						break
-			if flag =="Found":
-				src_positions.append(pos)
-
-		print(src_positions)
-		print(trg_positions)
-
-
-		for sp,tp in zip(src_positions,trg_positions):
-			print(sp)
-			print(tp)
-			verseID=re.split("_",sp)[0]
-			cur.execute("DELETE FROM "+self.alignment_table_name+"  WHERE source_wordID='"+sp+"'")
-
-			verseID=re.split("_",tp)[0]
-			cur.execute("DELETE FROM "+self.alignment_table_name+"  WHERE target_wordID='"+tp+"'")
-
-			cur.execute("INSERT INTO "+self.alignment_table_name+" (source_wordID,target_wordID,corrected) VALUES (%s,%s,0)",
-				(sp,tp))
+		for pair in word_pairs:
+			cur.execute("INSERT INTO "+self.alignment_table_name+" (lid,source_wordID,target_wordID,user) VALUES (%s,%s,%s,'default')",
+				(lid, pair[0],pair[1]))
 
 		self.db.commit()
 
+
+	
 
 
 	def update_alignment_on_verse(self,lid_to_update):
@@ -175,7 +138,7 @@ class FeedbackAligner:
 		replacement_options=[]
 		mapped = []
 		for s_wrd in src_word_list:
-			cur.execute("SELECT target_word FROM "+self.FeedbackLookup_table_name+" WHERE source_word='"+s_wrd[0]+"' ORDER BY confidence_score")
+			cur.execute("SELECT target_word FROM "+self.FeedbackLookup_table_name+" WHERE source_word='"+s_wrd[0]+"' ORDER BY confidence_score DESC")
 			for row in cur.fetchall():
 				mapped_trg_word = row[0]
 				flag = False
@@ -211,13 +174,60 @@ class FeedbackAligner:
 
 
 
-
-	def mark_alignment_as_verified(self,lid):
+	def fetch_alignment(self,lid,auto_alignmenttable):
 		cur = self.db.cursor()
 
-		cur.execute("UPDATE "+self.alignment_table_name+" SET corrected=1 WHERE source_wordID LIKE '"+str(lid)+"\_%'")
-		self.db.commit()
+		# get source_word list and target list for the verse from concordance table
+		cur.execute("SELECT word,occurences FROM "+self.src_table_name+" WHERE occurences LIKE '"+lid+"\_%'")
+		src_word_list = []
+		for row in cur.fetchall():
+			src_word_list.append((row[0],row[1]))
+		print("src_word_list")
+		print(src_word_list)
 
+
+		cur.execute("SELECT word,occurences FROM "+self.trg_table_name+" WHERE occurences LIKE '"+lid+"\_%'")
+		trg_word_list = []
+		for row in cur.fetchall():
+			trg_word_list.append((row[0],row[1]))
+		print("trg_word_list")
+		print(trg_word_list)
+
+
+		# get the alignments from specified table
+		cur.execute("SELECT * from "+auto_alignmenttable+" WHERE lid='"+lid+"'")
+		auto_alignments = cur.fetchall()
+		print("auto_alignments")
+		print(auto_alignments)
+
+		# get the alignments from master table, if present
+		cur.execute("SELECT * from "+self.alignment_table_name+" WHERE lid='"+lid+"'")
+		corrected_alignments = cur.fetchall()
+		print("corrected_alignments")
+		print(corrected_alignments)
+
+		# for all source_words get _FeedbackLookup entry
+		# and if entry present in trg_verse, add them to replacement_options
+		replacement_options=[]
+		mapped = []
+		for s_wrd in src_word_list:
+			cur.execute("SELECT target_word FROM "+self.FeedbackLookup_table_name+" WHERE source_word='"+s_wrd[0]+"' ORDER BY confidence_score DESC")
+			for row in cur.fetchall():
+				mapped_trg_word = row[0]
+				flag = False
+				for t_wrd in trg_word_list:
+					if t_wrd[0]==mapped_trg_word and t_wrd[1] not in mapped:
+						replacement_options.append((s_wrd[1],t_wrd[1]))
+						mapped.append(t_wrd[1])
+						flag=True 
+						break
+				if flag==True:
+					break
+		print("replacement_options")
+		print(replacement_options)
+
+		# return both
+	
 
 
 	def on_approve_feedback(self,corrected_src_trg_word_list):
@@ -254,9 +264,9 @@ if __name__ == '__main__':
 
 	obj.on_approve_feedback([("G24240","यीशु"),("G52070","सन्तान")])
 
-	# obj.mark_alignment_as_verified(23147)
+	obj.fetch_alignment('23146','grk_hin_sw_stm_ne_giza_tw__alignment')
 
-	# obj.update_alignment_on_verse('23146')
+	# obj.update_al
 	
 
 	del obj
