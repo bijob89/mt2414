9cb976a4860bdb17c1bdddfb6dd4554060ae3f9b
kavitharaju <kavitharaju18@gmail.com>
2018-08-08 22:50:57 +0530

    Multi words in Feedback table
    -m source words can be mapped to n target words in Feedback look up. Updated
         - the insert into lookup table
         - update verse using lookup table
    - changed the confidence score formula within the feedback lookup table

--- FeedbackAligner.py
+++ FeedbackAligner.py
@@ -4,6 +4,7 @@
 
 import re, sys, pickle
 import os.path 
+import itertools
 import pymysql
 
 
@@ -45,7 +46,7 @@ class FeedbackAligner:
 			cur.execute("ALTER TABLE "+self.FeedbackLookup_table_name+" ADD INDEX target_word_index(target_word)")
 			self.db.commit()
 		
-	# def __del__(self):
+	def __del__(self):
 		
 		self.db.commit()
 		# self.db.close()
@@ -56,49 +57,85 @@ class FeedbackAligner:
 		try:	
 			cur = self.db.cursor()
 
+			src_word_array = src_word.split(" ")
+			trg_word_array = trg_word.split(" ")
+
 
 			# for calculating confidence_score
-			cur.execute("select count(distinct left(occurences,5)) from "+self.src_table_name+" where word='"+src_word+"'") 
-			if cur.rowcount>0:
-				total_verses_src_word_occured = cur.fetchone()[0]
-				print(total_verses_src_word_occured)
-			else:
+
+			#handling multi-word src phrases and multi-word non-contiguous phrases
+			#multi-words inputed as, a single string with "space" seperating the words
+			#non-contigious words, as a single string with "." for every word in between and all words and dots separated by "space"
+			cur.execute("select occurences  from "+self.src_table_name+" where word='"+src_word_array[0]+"'")
+			src_word_occurences = [x[0] for x in cur.fetchall()]
+			for i,src_word in enumerate(src_word_array):
+				if src_word == ".":
+					continue
+				for fetched_pos in src_word_occurences:
+					lid = fetched_pos.split("_")[0]
+					pos = int(fetched_pos.split("_")[1])
+					pos = pos + i
+					check_occurance = lid+"_"+str(pos)
+					cur.execute("select word from "+self.src_table_name+" where word='"+src_word+"' and occurences='"+check_occurance+"'")
+					if cur.rowcount==0:
+						src_word_occurences.remove(fetched_pos)
+
+			total_verses_src_word_occured = len(src_word_occurences)
+			if total_verses_src_word_occured==0:
 				total_verses_src_word_occured = 1
 
-			query = " select count(distinct left("+ self.trg_table_name +".occurences,5)) from "+ self.trg_table_name +" INNER JOIN "+ self.src_table_name +" on left("+self.trg_table_name+".occurences,5)=left("+self.src_table_name+".occurences,5) where "+self.src_table_name+".word=\'"+src_word+"\' and "+self.trg_table_name+".word=\'"+trg_word+"\'"
-			print(query)
-			cur.execute(query)
-			if cur.rowcount>0:
-				total_verses_trg_word_cooccured = cur.fetchone()[0]
-				print(total_verses_trg_word_cooccured)
-			else:
-				total_verses_trg_word_cooccured = 0
+			
+
+			cur.execute("select occurences  from "+self.trg_table_name+" where word='"+trg_word_array[0]+"'")
+			trg_word_occurences = [x[0] for x in cur.fetchall()]
+			for i,trg_word in enumerate(trg_word_array):
+				if trg_word == ".":
+					continue
+				for fetched_pos in trg_word_occurences:
+					lid = fetched_pos.split("_")[0]
+					pos = int(fetched_pos.split("_")[1])
+					pos = pos + i
+					check_occurance = lid+"_"+str(pos)
+					cur.execute("select word from "+self.trg_table_name+" where word='"+trg_word+"' and occurences='"+check_occurance+"'")
+					if cur.rowcount==0:
+						trg_word_occurences.remove(fetched_pos)
+
+			src_lids = [x.split("_")[0] for x in src_word_occurences]
+			trg_lids = [x.split("_")[0] for x in trg_word_occurences]
+			
+			co_occurence_lid_set = set(src_lids).intersection(set(trg_lids))
+			total_verses_trg_word_cooccured = len(co_occurence_lid_set)
+			
+			co_occurence_pospair_set =[]
+			for x,y in itertools.product(src_word_occurences,trg_word_occurences):
+				if x.split("_")[0]==y.split("_")[0]:
+					co_occurence_pospair_set.append((x,y))
+			
 
 
-			cur.execute("	select count(distinct b.lid) from "+self.src_table_name+" a, "+self.alignment_table_name+" b, "+self.trg_table_name+" c where a.occurences=b.source_wordID and c.occurences=b.target_wordID and a.word='"+src_word+"' and c.word='"+trg_word+"'")
-			if cur.rowcount>0:
-				total_verses_src_trg_aligned = cur.fetchone()[0]
-				print(total_verses_src_trg_aligned)
-			else:
-				total_verses_src_trg_aligned = 1
+			cur.execute("SELECT distinct lid from "+self.alignment_table_name)
+			verses_in_aligned_table = [str(x[0]) for x in cur.fetchall()]
+			total_cooccurance_verses_INalignedtable = len(co_occurence_lid_set.intersection(set(verses_in_aligned_table)))
 
-
-			cur.execute("	select count(distinct b.lid) from "+self.src_table_name+" a, "+self.alignment_table_name+" b, "+self.trg_table_name+" c where a.occurences=b.source_wordID and c.occurences=b.target_wordID and a.word='"+src_word+"' and c.word!='"+trg_word+"'")
-			if cur.rowcount>0:
-				total_verses_src_trg_NOTaligned = cur.fetchone()[0]
-				print(total_verses_src_trg_NOTaligned)
-			else:
-				total_verses_src_trg_NOTaligned = 0
-
+			
+			if total_cooccurance_verses_INalignedtable==0:
+				total_cooccurance_verses_INalignedtable = 1
+			
+			total_verses_src_trg_aligned = 0
+			verses_src_trg_aligned = []
+			for pos_pair in co_occurence_pospair_set:
+				cur.execute("SELECT lid from "+self.alignment_table_name+" WHERE source_wordID='"+pos_pair[0]+"' and target_wordID='"+pos_pair[1]+"'")
+				if cur.rowcount>0:
+					verses_src_trg_aligned.append(cur.fetchone()[0])
+			total_verses_src_trg_aligned = len(set(verses_src_trg_aligned))
+			
 			co_occurence_confidence = total_verses_trg_word_cooccured/total_verses_src_word_occured
-			aligned_confidence = (total_verses_src_trg_aligned-total_verses_src_trg_NOTaligned) /(total_verses_src_trg_aligned+total_verses_src_trg_NOTaligned)
-
-
+			aligned_confidence = total_verses_src_trg_aligned / total_cooccurance_verses_INalignedtable
 			confidence_score = 0.75*co_occurence_confidence + 0.25*aligned_confidence
 	
-
+			print(" ".join(src_word_array)+"---"+" ".join(trg_word_array)+":"+str(confidence_score))
 			cur.execute("INSERT INTO "+self.FeedbackLookup_table_name+" (source_word,target_word,confidence_score) VALUES (%s,%s,%s)",
-				(src_word,trg_word,confidence_score))
+				(" ".join(src_word_array)," ".join(trg_word_array),confidence_score))
 			self.db.commit()
 			
 		except Exception as e:
@@ -137,19 +174,58 @@ class FeedbackAligner:
 		# and if entry present in trg_verse, add them to replacement_options
 		replacement_options=[]
 		mapped = []
-		for s_wrd in src_word_list:
-			cur.execute("SELECT target_word FROM "+self.FeedbackLookup_table_name+" WHERE source_word='"+s_wrd[0]+"' ORDER BY confidence_score DESC")
+		for i,s_wrd in enumerate(src_word_list):
+
+			cur.execute("SELECT source_word, target_word FROM "+self.FeedbackLookup_table_name+" WHERE source_word like '"+s_wrd[0]+"%' ORDER BY  confidence_score DESC, CHAR_LENGTH(source_word) DESC")
 			for row in cur.fetchall():
-				mapped_trg_word = row[0]
-				flag = False
-				for t_wrd in trg_word_list:
-					if t_wrd[0]==mapped_trg_word and t_wrd[1] not in mapped:
-						replacement_options.append((s_wrd[1],t_wrd[1]))
-						mapped.append(t_wrd[1])
-						flag=True 
-						break
-				if flag==True:
+				drop = False
+				mapped_src_word = row[0]
+				mapped_trg_word = row[1]
+				src_pos = []
+				src_multiword_list = mapped_src_word.split(" ")
+				for j,w in enumerate(src_multiword_list):
+					if (i+j)<len(src_word_list) and  src_word_list[i+j][0]==w :
+						src_pos.append(src_word_list[i+j][1])
+					elif (i+j)<len(src_word_list) and w==".":
+						pass
+					else:
+						drop=True
+				
+				if drop:
 					break
+
+				trg_multiword_list = mapped_trg_word.split(" ")
+				trg_pos = []
+				for i,t_wrd in enumerate(trg_word_list):
+					if t_wrd[1] in mapped:
+						continue
+					temp_trg_pos = []
+					drop = False
+					for j,w in enumerate(trg_multiword_list):
+						if (i+j)<len(trg_word_list) and  trg_word_list[i+j][0] == w :
+							temp_trg_pos.append(trg_word_list[i+j][1])
+						elif (i+j)<len(trg_word_list) and w==".":
+							pass
+						else:
+							temp_trg_pos = []
+							drop = True
+							break
+					if not drop:
+						trg_pos = temp_trg_pos
+						break
+				if drop:
+					break
+
+
+
+				for i,j in itertools.product(src_pos,trg_pos):
+					replacement_options.append((i,j))
+					mapped.append(j)
+					drop = True
+				# print('replacement_options'+str(replacement_options))
+				if drop:
+					break
+
 		print(replacement_options)
 
 
@@ -162,14 +238,14 @@ class FeedbackAligner:
 
 		for pair in replacement_options:
 
-			cur.execute("INSERT INTO "+self.alignment_table_name+" (lid,source_wordID,target_wordID,corrected) VALUES (%s,%s,%s,0)",
+			cur.execute("INSERT INTO "+self.alignment_table_name+" (lid,source_wordID,target_wordID,user) VALUES (%s,%s,%s,'default')",
 				(lid_to_update, pair[0],pair[1]))
 
 		self.db.commit()
 
 			
 
-		# for word 
+		
 
 
 
@@ -262,11 +338,11 @@ if __name__ == '__main__':
 	obj = FeedbackAligner(connection, src,trg)
 
 
-	obj.on_approve_feedback([("G24240","यीशु"),("G52070","सन्तान")])
+	# obj.on_approve_feedback([("2424 5547","यीशु मसीह"),("5207","सन्तान"),("5257 5547","मसीह . सेवक")])
 
-	obj.fetch_alignment('23146','grk_hin_sw_stm_ne_giza_tw__alignment')
+	# obj.fetch_alignment('23146','grk_hin_sw_stm_ne_giza_tw__alignment')
 
-	# obj.update_al
+	obj.update_alignment_on_verse('23146')
 	
 
 	del obj
